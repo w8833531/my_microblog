75c8488ad2b78f896bd3ff511404f89cbaf29314
w8833531 <w8833531@hotmail.com>
2018-07-20 14:11:59 +0800

    增加 jinja2 模板及模板继承，完善index页面

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,22 @@
 # @Author : 吴鹰 (wuying)
 # @Link   : 
 # @Date   : 7/19/2018, 2:31:18 PM
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username': 'LarryWu'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
